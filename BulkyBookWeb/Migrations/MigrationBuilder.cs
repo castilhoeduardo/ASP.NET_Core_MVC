5eafe405bb63a740c8ae64e934a77d80a85c615a
castilhoeduardo <alcantara.ecb@gmail.com>
2023-07-14 11:06:09 -0300

    ..

---
+++ BulkyBookWeb/Migrations/MigrationBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BulkyBookWeb.Migrations
+{
+    public class MigrationBuilder
+    {
+        internal void CreateTable(string name, Func<object, object> columns, Action<object> constraints)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void DropTable(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
